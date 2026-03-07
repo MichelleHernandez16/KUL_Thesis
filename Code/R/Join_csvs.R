c8f6732288402efd674cdba9d98510948e6bbd32
Michelle Hernandez <michelle.hernandez@student.kuleuven.be>
2026-03-07 17:34:02 +0100

    Update final datasets and codes

--- Code/R/Join_csvs.R
+++ Code/R/Join_csvs.R
@@ -11,10 +11,11 @@ setwd("C:/git/Thesis/Data/Premier/Premier_revised")
 
 #Load libraries
 library(dplyr)
+library(stringr)
+library(lubridate)
 
 #Load the csvs
-# premier_df <- read.csv("premier_league_player_stats_cleaned.csv")
-# debut_df <- read.csv("premier_players_debut.csv")
+3# debut_df <- read.csv("premier_players_debut.csv")
 # transfer_df <- read.csv("players_transfermarkt.csv")
 # fbref_df <- read.csv("premier_fbref.csv")
 
@@ -43,7 +44,7 @@ premier_all <- premier_df %>%
 
 
 head(premier_all)
-names(premier_)
+names(premier_all)
 
 
 
@@ -65,19 +66,18 @@ all_almost_df <- read.csv("premier_all2.csv")
  
  
 
- 
+ #Make summary by player
  career_summary <- national_df %>%
    group_by(player_url, player_name) %>%
    summarise(
-     total_matches = sum(matches_played, na.rm = TRUE),
-     total_minutes = sum(minutes, na.rm = TRUE),
-     total_goals = sum(goals, na.rm = TRUE),
-     total_assists = sum(assists, na.rm = TRUE),
-     seasons_played = n(),  #
+    matches_national = sum(matches_played, na.rm = TRUE),
+    minutes_national = sum(minutes, na.rm = TRUE),
+    goals_national = sum(goals, na.rm = TRUE),
+    seasons_national       = n_distinct(season),   
      .groups = "drop"
    )
  
- 
+
  
  
  #join to my main dataframe
@@ -92,19 +92,17 @@ history_df <- read.csv("premier_career_history.csv")
 
 
 
+ 
 history_summary <- history_df %>%
-  group_by(player_name) %>%
+  group_by(player_url) %>%
   summarise(
     total_matches_club = sum(matches_played, na.rm = TRUE),
     total_minutes_club = sum(minutes, na.rm = TRUE),
     total_goals_club   = sum(goals, na.rm = TRUE),
-    total_assists_club = sum(assists, na.rm = TRUE),
-    seasons_club       = n(),         
+    seasons_club       = n_distinct(season),   # seasons
+    clubs_club         = n_distinct(squad),    # disctinct clubs
     .groups = "drop"
   )
-
- 
-
 
 
 
@@ -112,14 +110,27 @@ names(premier_all2)
 names(history_summary)
 
 
-premier_all2 <- premier_all2 %>%
-  rename(player_name = player_name.x)
 
 
 premier_all3 <- premier_all2 %>%
   left_join(history_summary,
-            by = "player_name",
+            by = c("fbref_url" = "player_url"),
             relationship = "many-to-many")
+
+
+
+###NOW Add if they are deceased
+deceased_df <- read.csv("players_with_deceased_status.csv")
+
+
+
+premier_all3 <- premier_all3 %>%
+  left_join(deceased_df, by = "transfermarkt_url", relationship = "many-to-many")
+
+
+
+
+
 
 
 #save csv
@@ -127,3 +138,347 @@ write.csv(premier_all3,
           "C:/git/Thesis/Data/Premier/Premier_revised/final_dataset/premier_all3.csv",
           row.names = FALSE)
 file.exists("C:/git/Thesis/Data/Premier/Premier_revised/final_dataset/premier_all3.csv")
+
+
+
+
+### CONSTRUCTED COLUMNS
+
+
+#Sum of national team performance and all competitions performance metrics to have a total 
+premier_all4 <- premier_all3 %>%
+  mutate(
+    total_matches_total = coalesce(matches_national, 0) + coalesce(total_matches_club, 0),
+    total_minutes_total = coalesce(minutes_national, 0) + coalesce(total_minutes_club, 0),
+    total_goals_total   = coalesce(goals_national,   0) + coalesce(total_goals_club,   0)
+  )
+
+
+
+#Simplify the positions 
+premier_all4 <- premier_all4 %>%
+  mutate(
+    position_clean = str_squish(position.x.x),  # clean
+    position_simple = case_when(
+      str_detect(position_clean, "Goalkeeper") ~ "Goalkeeper",
+      str_detect(position_clean, "Attack|Winger|Second Striker|Centre-Forward") ~ "Attack",
+      str_detect(position_clean, "Midfield") ~ "Midfield",
+      str_detect(position_clean, "Defender|Centre-Back|Right-Back|Left-Back") ~ "Defender",
+      TRUE ~ "Other"
+    )
+  )
+
+
+
+#current age and retirement age
+
+premier_all4 <- premier_all4 %>%
+  mutate(
+    dob = dmy(dob_age.x)   # change to date
+  )
+
+
+premier_all4 <- premier_all4 %>%
+  mutate(
+    age_current = floor(interval(dob, today()) / years(1))
+  )
+
+
+premier_all4 <- premier_all4 %>%
+  mutate(
+    retirement_date = na_if(trimws(retirement_date), ""),
+    retirement_date_clean = dmy(retirement_date, quiet = TRUE)
+  )
+
+
+premier_all4 <- premier_all4 %>%
+  mutate(
+    retirement_age = case_when(
+      retired == "Yes" & !is.na(dob) & !is.na(retirement_date_clean) ~
+        floor(interval(dob, retirement_date_clean) / years(1)),
+      TRUE ~ NA_real_
+    )
+  )
+
+
+#Now career_lenght
+premier_all4 <- premier_all4 %>%
+  mutate(
+    debut_age_transfermarket = as.numeric(debut_age_transfermarket),
+    death_age = as.numeric(death_age)   
+  )
+
+unique(premier_all4$debut_age_transfermarket[is.na(as.numeric(premier_all4$debut_age_transfermarket))])
+
+
+
+premier_all4 <- premier_all4 %>%
+  mutate(
+    career_length = case_when(
+      
+      # 1. Retired players
+      retired == "Yes" & !is.na(retirement_age) ~
+        retirement_age - debut_age_transfermarket,
+      
+      # 2. Not retired but dead
+      deceased == "Yes" & !is.na(death_age) ~
+        death_age - debut_age_transfermarket,
+      
+      # 3. Still playing
+      retired != "Yes" & deceased != "Yes" & !is.na(age_current) ~
+        age_current - debut_age_transfermarket,
+      
+      # 4. Others
+      TRUE ~ NA_real_
+    )
+  )
+
+
+
+#save csv
+write.csv(premier_all4,
+          "C:/git/Thesis/Data/Premier/Premier_revised/final_dataset/premier_all4.csv",
+          row.names = FALSE)
+file.exists("C:/git/Thesis/Data/Premier/Premier_revised/final_dataset/premier_all4.csv")
+
+
+
+
+
+
+
+
+
+
+##OK NOW FOR NOT DETAILED DATA
+
+
+
+#Load the csvs (THIS PART IS FOR VALIDATED PLAYERS AFTER CHECKING TRANSFER ID)
+#premier_df_2 <- read.csv("premier_league_player_stats_cleaned_revised.csv")
+debut_df_2 <- read.csv("premier_players_debut_2.csv")
+transfer_df_2 <- read.csv("players_transfermarkt_2.csv")
+fbref_df_2 <- read.csv("premier_fbref2.csv")
+
+id_lookup <- premier_df %>% 
+  select(transfermarkt_url, fbref_url) %>%
+  distinct()
+
+
+
+#JOINT THE CVS BUT NOW BY THE transfermarkt_url
+premier_all <- premier_df %>%
+  left_join(debut_df, by = "transfermarkt_url", relationship = "many-to-many") %>%
+  left_join(transfer_df, by = "transfermarkt_url", relationship = "many-to-many") %>%
+  left_join(fbref_df, by = "fbref_url", relationship = "many-to-many")
+
+premier_simple <- debut_df %>%
+  left_join(transfer_df, by = "transfermarkt_url", relationship = "many-to-many") %>%
+  left_join(id_lookup,   by = "transfermarkt_url") %>%  # adds fbref_url
+  left_join(fbref_df,    by = "fbref_url", relationship = "many-to-many")
+
+
+
+
+
+#save csv
+#write.csv(premier_all,
+"C:/git/Thesis/Data/Premier/Premier_revised/premier_all2.csv",
+row.names = FALSE)
+file.exists("C:/git/Thesis/Data/Premier/Premier_revised/premier_all2.csv")
+
+
+
+
+
+###NOW ADD THE PERFORMANCE SUMMARIES
+
+
+
+national_df <- read.csv("premier_national_team.csv")
+
+
+
+
+career_summary <- national_df %>%
+  group_by(player_url, player_name) %>%
+  summarise(
+    matches_national = sum(matches_played, na.rm = TRUE),
+    minutes_national = sum(minutes, na.rm = TRUE),
+    goals_national = sum(goals, na.rm = TRUE),
+    seasons_national       = n_distinct(season),   
+    .groups = "drop"
+  )
+
+
+
+
+#join to my main dataframe
+premier_simple2 <- premier_simple %>%
+  left_join(career_summary,
+            by = c("fbref_url" = "player_url"),
+            relationship = "many-to-many")
+
+
+##All competitions history
+history_df <- read.csv("premier_career_history.csv")
+
+
+
+
+history_summary <- history_df %>%
+  group_by(player_url) %>%
+  summarise(
+    total_matches_club = sum(matches_played, na.rm = TRUE),
+    total_minutes_club = sum(minutes, na.rm = TRUE),
+    total_goals_club   = sum(goals, na.rm = TRUE),
+    seasons_club       = n_distinct(season),   # seasons
+    clubs_club         = n_distinct(squad),    # disctinct clubs
+    .groups = "drop"
+  )
+
+
+
+names(premier_all2)
+names(history_summary)
+
+
+
+
+premier_simple2 <- premier_simple2 %>%
+  left_join(history_summary,
+            by = c("fbref_url" = "player_url"),
+            relationship = "many-to-many")
+
+
+
+###NOW Add if they are deceased
+deceased_df <- read.csv("players_with_deceased_status.csv")
+
+
+
+premier_simple2 <- premier_simple2 %>%
+  left_join(deceased_df, by = "transfermarkt_url", relationship = "many-to-many")
+
+
+
+
+
+
+
+#save csv
+write.csv(premier_simple2,
+          "C:/git/Thesis/Data/Premier/Premier_revised/final_dataset/premier_simple.csv",
+          row.names = FALSE)
+#file.exists("C:/git/Thesis/Data/Premier/Premier_revised/final_dataset/premier_all3.csv")
+
+
+
+
+### CONSTRUCTED COLUMNS
+
+
+#Sum of national team performance and all competitions performance metrics to have a total 
+premier_simple3 <- read.csv("premier_simple.csv")
+
+premier_simple3 <- premier_simple3 %>%
+  mutate(
+    total_matches_total = coalesce(matches_national, 0) + coalesce(total_matches_club, 0),
+    total_minutes_total = coalesce(minutes_national, 0) + coalesce(total_minutes_club, 0),
+    total_goals_total   = coalesce(goals_national,   0) + coalesce(total_goals_club,   0)
+  )
+
+
+
+#Simplify the positions 
+premier_simple3 <- premier_simple3 %>%
+  mutate(
+    position_clean = str_squish(position.x),  # clean
+    position_simple = case_when(
+      str_detect(position_clean, "Goalkeeper") ~ "Goalkeeper",
+      str_detect(position_clean, "Attack|Winger|Second Striker|Centre-Forward") ~ "Attack",
+      str_detect(position_clean, "Midfield") ~ "Midfield",
+      str_detect(position_clean, "Defender|Centre-Back|Right-Back|Left-Back") ~ "Defender",
+      TRUE ~ "Other"
+    )
+  )
+
+
+
+#current age and retirement age
+unique(premier_simple3$dob_age.x[1:50])
+
+premier_simple3 <- premier_simple3 %>%
+  mutate(
+    dob = dmy(dob_age.x)   # change to date
+  )
+
+
+premier_simple3 <- premier_simple3 %>%
+  mutate(
+    age_current = floor(interval(dob, today()) / years(1))
+  )
+
+
+premier_simple3 <- premier_simple3 %>%
+  mutate(
+    retirement_date = na_if(trimws(retirement_date), ""),
+    retirement_date_clean = dmy(retirement_date, quiet = TRUE)
+  )
+
+
+premier_simple3 <- premier_simple3 %>%
+  mutate(
+    retirement_age = case_when(
+      retired == "Yes" & !is.na(dob) & !is.na(retirement_date_clean) ~
+        floor(interval(dob, retirement_date_clean) / years(1)),
+      TRUE ~ NA_real_
+    )
+  )
+
+
+#Now career_lenght
+premier_simple3 <- premier_simple3 %>%
+  mutate(
+    debut_age_transfermarket = as.numeric(debut_age_transfermarket),
+    death_age = as.numeric(death_age)   
+  )
+
+unique(premier_all4$debut_age_transfermarket[is.na(as.numeric(premier_all4$debut_age_transfermarket))])
+
+
+
+premier_simple3 <- premier_simple3 %>%
+  mutate(
+    career_length = case_when(
+      
+      # 1. Retired players
+      retired == "Yes" & !is.na(retirement_age) ~
+        retirement_age - debut_age_transfermarket,
+      
+      # 2. Not retired but dead
+      deceased == "Yes" & !is.na(death_age) ~
+        death_age - debut_age_transfermarket,
+      
+      # 3. Still playing
+      retired != "Yes" & deceased != "Yes" & !is.na(age_current) ~
+        age_current - debut_age_transfermarket,
+      
+      # 4. Others
+      TRUE ~ NA_real_
+    )
+  )
+
+
+
+#save csv
+write.csv(premier_simple3,
+          "C:/git/Thesis/Data/Premier/Premier_revised/final_dataset/premier_simple.csv",
+          row.names = FALSE)
+file.exists("C:/git/Thesis/Data/Premier/Premier_revised/final_dataset/premier_simple.csv")
+
+
+
+
+
+
---
+++ Code/R/missing_columns.R
@@ -0,0 +1,162 @@
+########################################################
+###########    Join CSVS   ###################################
+########################################################
+
+##FILE TO JOIN THE LIST OF PLAYERS WITH THE DEBUT CSV AND THE GENERAL DATA CSV FROM TRANSFERMARKT
+
+
+
+
+#Load libraries
+library(dplyr)
+library(stringr)
+library(lubridate)
+
+
+### Load data
+-
+
+# Adjust path if needed; here assumed in working directory
+df<- read_csv("la_liga_stats_2010_2015_depured.csv",
+                show_col_types = FALSE)
+names(df)
+
+###NOW ADD THE PERFORMANCE SUMMARIES
+### CONSTRUCTED COLUMNS
+str(df2$total_seasons_national)
+str(df$`CAREER Sum of goals`)
+str(df2$matches_national)
+str(df2$`CAREER Sum of matches_played`)
+
+
+
+#I need to change my national team variables as they are not numeric
+national_cols <- c("matches_national", "minutes_national", "goals_national", "total_seasons_national")
+
+df2 <- df %>%
+  mutate(
+    across(
+      all_of(national_cols),
+      ~ parse_number(replace(as.character(.), . == "Not_national_team", "0")),
+      .names = "{.col}_num"
+    )
+  )
+
+#replace back
+df2 <- df %>%
+  mutate(
+    across(
+      all_of(national_cols),
+      ~ parse_number(replace(as.character(.), . == "Not_national_team", "0"))
+    )
+  )
+
+
+#Sum of national team performance and all competitions performance metrics to have a total 
+df2 <- df2 %>%
+  mutate(
+    total_matches_total = coalesce(matches_national, 0) + coalesce(`CAREER Sum of matches_played`, 0),
+    total_minutes_total = coalesce(minutes_national, 0) + coalesce(`CAREER Sum of minutes`, 0),
+    total_goals_total   = coalesce(goals_national,   0) + coalesce(`CAREER Sum of goals`,   0)
+  ) # need to indicate the column with backticks because of spaces in the names
+
+
+
+
+#Simplify the positions 
+df2 <- df2 %>%
+  mutate(
+    position_clean = str_squish(position_tra),  # clean
+    position_simple = case_when(
+      str_detect(position_clean, "Goalkeeper") ~ "Goalkeeper",
+      str_detect(position_clean, "Attack|Winger|Second Striker|Centre-Forward") ~ "Attack",
+      str_detect(position_clean, "Midfield") ~ "Midfield",
+      str_detect(position_clean, "Defender|Centre-Back|Right-Back|Left-Back") ~ "Defender",
+      TRUE ~ "Other"
+    )
+  )
+
+
+
+#current age and retirement age
+
+df2 <- df2 %>%
+  mutate(
+    dob = dmy(dob_age_tra)   # change to date
+  )
+
+
+df2 <- df2 %>%
+  mutate(
+    age_current = floor(interval(dob, today()) / years(1))
+  )
+
+
+df2 <- df2 %>%
+  mutate(
+    retirement_date = na_if(trimws(retirement_date_tra), ""),
+    retirement_date_clean = dmy(retirement_date_tra, quiet = TRUE)
+  )
+
+
+df2 <- df2 %>%
+  mutate(
+    retirement_age = case_when(
+      retired_tra == "Yes" & !is.na(dob) & !is.na(retirement_date_clean) ~
+        floor(interval(dob, retirement_date_clean) / years(1)),
+      TRUE ~ NA_real_
+    )
+  )
+
+
+#Now career_lenght
+df2 <- df2 %>%
+  mutate(
+    debut_age_transfermarket = as.numeric(debut_age_transfermarket),
+    death_age = as.numeric(death_age)   
+  )
+
+unique(df2$debut_age_transfermarket[is.na(as.numeric(df2$debut_age_transfermarket))])
+
+
+
+df2 <- df2 %>%
+  mutate(
+    career_length = case_when(
+      
+      # 1. Retired players
+      retired_tra == "Yes" & !is.na(retirement_age) ~
+        retirement_age - debut_age_transfermarket,
+      
+      # 2. Not retired but dead
+      Deceased_tra == "Yes" & !is.na(death_age) ~
+        death_age - debut_age_transfermarket,
+      
+      # 3. Still playing
+      retired_tra != "Yes" & Deceased_tra != "Yes" & !is.na(age_current) ~
+        age_current - debut_age_transfermarket,
+      
+      # 4. Others
+      TRUE ~ NA_real_
+    )
+  )
+
+
+
+#save csv
+write.csv(df2,
+          "C:/git/Thesis/Data/la_liga_stats_2010_2015_depured2.csv",
+          row.names = FALSE)
+file.exists( "C:/git/Thesis/Data/la_liga_stats_2010_2015_depured2.csv")
+
+
+
+
+
+
+
+
+
+
+
+
